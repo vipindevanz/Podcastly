ae7d277489be6e85946c617dfb1c6274e5daa9de
Vipin dev <officialvipindev@gmail.com>
2021-11-10 12:30:08 +0530

    Added google authentication

--- app/src/main/java/com/pns/podcastly/ui/activity/MainActivity.kt
+++ app/src/main/java/com/pns/podcastly/ui/activity/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.pns.podcastly
+package com.pns.podcastly.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pns.podcastly.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
---
+++ app/src/main/java/com/pns/podcastly/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.pns.podcastly.utils
+
+object Constants {
+
+    const val RC_SIGN_IN = 100
+    const val GOOGLE_AUTH_TAG = "GOOGLE_SIGN_IN_TAG"
+    const val DEBUG_TAG = "com.pns.podcastly"
+}
