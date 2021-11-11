369fe602d85d6c5e986dacdb7e2b96b636315f2c
Prabhakar <prabhakarkr802@gmail.com>
2021-11-11 14:06:41 +0530

    Navigation

--- app/src/main/java/com/pns/podcastly/MainActivity.kt
+++ app/src/main/java/com/pns/podcastly/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.pns.podcastly
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.pns.podcastly.fragment.HomeFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNav.background = null
+        bottomNav.menu.getItem(2).isEnabled = false
+
+        setCurrentFragment(HomeFragment())
+
+        // Navigate to Fragment
+        bottomNav.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> setCurrentFragment(HomeFragment())
+            }
+            true
+        }
+    }
+
+
+    private fun setCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment)
+            .addToBackStack("$fragment").commit()
     }
 }
