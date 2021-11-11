fa1d52f7aea5025f0c9aec9367514f71eb54c151
Vipin dev <officialvipindev@gmail.com>
2021-11-11 18:36:30 +0530

    Implement Audio recording

--- app/src/main/java/com/pns/podcastly/utils/Constants.kt
+++ app/src/main/java/com/pns/podcastly/utils/Constants.kt
@@ -3,6 +3,6 @@ package com.pns.podcastly.utils
 object Constants {
 
     const val RC_SIGN_IN = 100
-    const val GOOGLE_AUTH_TAG = "GOOGLE_SIGN_IN_TAG"
     const val DEBUG_TAG = "com.pns.podcastly"
+    const val MICROPHONE_REQUEST_CODE = 4
 }
---
+++ app/src/main/java/com/pns/podcastly/utils/WaveFormView.kt
@@ -0,0 +1,68 @@
+package com.pns.podcastly.utils
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
+import android.util.AttributeSet
+import android.view.View
+
+class WaveFormView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+
+    private var paint = Paint()
+    private var amplitudes = ArrayList<Float>()
+    private var spikes = ArrayList<RectF>()
+
+    private var radius = 6f
+    private var w = 9f
+    private var d = 6f
+
+    private var sw = 0f
+    private var sh = 400f
+
+    private var maxSpikes = 0
+
+    init {
+        paint.color = Color.rgb(244, 81, 30)
+        sw = resources.displayMetrics.widthPixels.toFloat()
+
+        maxSpikes = (sw/(w+d)).toInt()
+    }
+
+    fun addAmplitudes(amp: Float) {
+        var norm = Math.min(amp.toInt()/7, 400).toFloat()
+        amplitudes.add(norm)
+
+        spikes.clear()
+
+        var amps = amplitudes.takeLast(maxSpikes)
+
+        for(i in amps.indices) {
+
+            var left = sw - i*(w+d)
+            var top = sh/2 - amps[i]/2
+            var right = left + w
+            var bottom = top + amps[i]
+            spikes.add(RectF(left, top, right, bottom))
+        }
+        invalidate()
+    }
+
+    fun clear() : ArrayList<Float> {
+
+        var amps = amplitudes.clone() as ArrayList<Float>
+        amplitudes.clear()
+        spikes.clear()
+        invalidate()
+
+        return amps
+    }
+
+    override fun draw(canvas: Canvas?) {
+        super.draw(canvas)
+        spikes.forEach {
+            canvas?.drawRoundRect(it, radius, radius, paint)
+        }
+    }
+}
