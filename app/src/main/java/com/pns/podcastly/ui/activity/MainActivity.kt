4990d232f9087a97c4d816af3d14068c3350fa7b
Prabhakar <prabhakarkr802@gmail.com>
2021-11-13 01:48:20 +0530

    MVVM

--- app/src/main/java/com/pns/podcastly/ui/activity/MainActivity.kt
+++ app/src/main/java/com/pns/podcastly/ui/activity/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.pns.podcastly.ui.activity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.pns.podcastly.R
+import com.pns.podcastly.remote.model.ListenNoteResponseDTO
+import com.pns.podcastly.repo.ListenNotesApiRepo
 import com.pns.podcastly.ui.fragment.HomeFragment
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-
+    private val podcastList = mutableListOf<ListenNoteResponseDTO>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,6 +30,8 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
+
+
     }
 
     private fun setCurrentFragment(fragment: Fragment) {
