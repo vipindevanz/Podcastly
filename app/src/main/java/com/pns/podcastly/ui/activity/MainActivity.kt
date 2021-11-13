0fb147304995e35d740389b8a616f9afc0100351
Vipin dev <officialvipindev@gmail.com>
2021-11-13 18:43:52 +0530

    Add some update

--- app/src/main/java/com/pns/podcastly/ui/activity/MainActivity.kt
+++ app/src/main/java/com/pns/podcastly/ui/activity/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         bottomNav.background = null
         bottomNav.menu.getItem(2).isEnabled = false
 
